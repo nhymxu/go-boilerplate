6d28e45766d33fd500293c6d695cd3c0694a60cc
Dung Nguyen <contact@dungnt.net>
2024-03-28 00:04:18 +0700

    refactor: adjust config file name

--- go.mod
+++ go.mod
@@ -38,7 +38,7 @@ require (
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/exp v0.0.0-20240318143956-a85f2c67cd81 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- pkg/config/env.go
+++ pkg/config/env.go
@@ -1,1 +1,49 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+// EnvConfigMap define mapping struct field and environment field
+type EnvConfigMap struct {
+	Debug  bool `mapstructure:"DEBUG"`
+	Sentry struct {
+		DSN string `mapstructure:"DSN"`
+	}
+}
+
+// ENV is global variable for using config in other place
+var ENV EnvConfigMap
+
+// LoadConfig read env file and loaded to environment and global ENV variable
+func LoadConfig(cfgFile string) error {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.AddConfigPath(".")
+		//viper.SetConfigType("yaml")
+		//viper.SetConfigName(".obm-bot-crawler")
+		viper.SetConfigFile(".env")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	err := viper.ReadInConfig()
+	if err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	} else {
+		return err
+	}
+
+	err = viper.Unmarshal(&ENV)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
